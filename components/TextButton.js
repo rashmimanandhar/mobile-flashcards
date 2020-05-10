57f44f3e186c5b117c043bac0912684bd66ca5d6
Rashmi <rashmi.manandhar7@gmail.com>
2020-05-10 11:50:31 -0400

    feat: create textbutton components

---
+++ components/TextButton.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Text, TouchableOpacity, View, StyleSheet} from 'react-native';
+
+export default function TextButton({children, onPress, btnStyle= {}, btnTextStyle = {}}) {
+  return(
+    <View style={[styles.btnContainer]}>
+      <TouchableOpacity style={[styles.btn, btnStyle]} onPress={onPress}>
+        <Text style={[styles.btnText, btnTextStyle]}>{children}</Text>
+      </TouchableOpacity>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  btnContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    // borderWidth: 1,
+    // margin: 10,
+    // borderColor: 'blue',
+    // borderRadius: 5
+  },
+  btnText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  btn:{
+    width: 200,
+    height: 50,
+    backgroundColor: 'red',
+    borderRadius: 5,
+    justifyContent: `center`,
+    alignItems: `center`
+  }
+})
+
+TextButton.propTypes = {
+  children: PropTypes.string.isRequired,
+  onPress: PropTypes.func.isRequired,
+  btnTextStyle: PropTypes.object,
+  btnStyle: PropTypes.object
+}
